0ba0b70aec52484fc22b5e78ccd24509aef1d36e
Arina Zhukova <Azhukova19842005@yandex.ru>
2025-11-09 00:41:21 +0300

    Начальная сборка

---
+++ app.js
@@ -0,0 +1,72 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load("particles-js", "assets/particles.json", function () {
+  console.log("callback - particles.js config loaded");
+});
+
+// Инициализация Swiper с эффектом карточек
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    // Дополнительные настройки (опционально)
+    loop: true,
+    // Настройки для эффекта карточек
+    cardsEffect: {
+      slideShadows: true, // Включить тени
+      rotate: true, // Вращение карточек
+      perSlideOffset: 8, // Смещение между карточками
+      perSlideRotate: 2, // Угол поворота карточек
+    },
+  });
+});
+
+const timer = (deadline) => {
+  const timerHours = document.getElementById("timer-hours");
+  const timerMinutes = document.getElementById("timer-minutes");
+  const timerSeconds = document.getElementById("timer-seconds");
+
+  let intervalId;
+
+  const getTimeRemaining = () => {
+    let dateStop = new Date(deadline).getTime();
+    let dateNow = new Date().getTime();
+
+    let timeRemaining = (dateStop - dateNow) / 1000;
+
+    let hours = Math.floor(timeRemaining / 60 / 60);
+    let minutes = Math.floor((timeRemaining / 60) % 60);
+    let secundes = Math.floor(timeRemaining % 60);
+    hours = createNumber(hours);
+    minutes = createNumber(minutes);
+    secundes = createNumber(secundes);
+
+    return { timeRemaining, hours, minutes, secundes };
+  };
+
+  const updateClock = () => {
+    let getTime = getTimeRemaining();
+
+    timerHours.textContent = getTime.hours;
+    timerMinutes.textContent = getTime.minutes;
+    timerSeconds.textContent = getTime.secundes;
+
+    if (getTime.timeRemaining <= 0) {
+      clearInterval(intervalId);
+      timerHours.textContent = "00";
+      timerMinutes.textContent = "00";
+      timerSeconds.textContent = "00";
+    }
+  };
+
+  const createNumber = (num) => {
+    if (num < 10) {
+      num = "0" + num;
+    }
+    return num;
+  };
+
+  intervalId = setInterval(updateClock, 1000);
+  updateClock();
+};
+
+timer("11 november 2025");
